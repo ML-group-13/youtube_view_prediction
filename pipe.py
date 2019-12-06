676d8806fde54a819c7265b0dabaf69e56beecc1
XabiKrant <xabikrant@live.nl>
2019-12-06 19:39:24 +0100

    First pipeline including a simple regression model using xgboost

---
+++ pipe.py
@@ -0,0 +1,36 @@
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import csv
+
+data_uk = pd.DataFrame(pd.read_csv("./data/GBvideos.csv", error_bad_lines=False))
+data_us = (pd.DataFrame(pd.read_csv("./data/USvideos.csv", error_bad_lines=False)))
+data = pd.concat([data_us, data_uk])
+
+print(data.columns)
+
+likes = data[['likes', 'dislikes', 'comment_total']]
+views = data[['views']]
+
+data_dmatrix = xgb.DMatrix(data=likes,label=views)
+
+likes_train, likes_test, views_train, views_test = train_test_split(likes, views, test_size=0.2, random_state=123)
+
+xg_reg = xgb.XGBRegressor(objective ='reg:linear', colsample_bytree = 0.3, learning_rate = 0.1,
+                max_depth = 5, alpha = 10, n_estimators = 10)
+
+xg_reg.fit(likes_train,views_train)
+
+preds = xg_reg.predict(likes_test)
+
+rmse = np.sqrt(mean_squared_error(views_test, preds))
+print("RMSE: %f" % (rmse))
+
+# Visualize feature importance
+xgb.plot_importance(xg_reg)
+plt.rcParams['figure.figsize'] = [5, 5]
+plt.show()
