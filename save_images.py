459f43986d67ec2aeebfb373551f726c95454232
XabiKrant <xabikrant@live.nl>
2019-12-09 22:02:15 +0100

    Added script to save images and the saved images

---
+++ save_images.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+import urllib
+import cv2
+
+data_uk = pd.DataFrame(pd.read_csv("./data/GBvideos.csv", error_bad_lines=False))
+data_us = pd.DataFrame(pd.read_csv("./data/USvideos.csv", error_bad_lines=False))
+data = pd.concat([data_us, data_uk])
+
+images = []
+thumbnails = data[['thumbnail_link']]
+thumbnails = thumbnails[:-15000]
+print(len(thumbnails))
+
+for i in range(0, len(thumbnails)):
+	thumbnail = thumbnails.values[i][0]
+	try:
+		resp = urllib.request.urlopen(thumbnail)
+		image = np.asarray(bytearray(resp.read()), dtype="uint8")
+		image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+		images.append(image)
+	except:
+		images.append(np.empty([1,1]))
+	if (i%1000 == 0) and (i > 0):
+		print(i)
+		np.savez("data/images/images_" + str(i-1000) + "_" + str(i) + ".npz", images)
+		images = []
+np.savez("data/images/images_" + str(15000) + "_" + str(15985) + ".npz", images)
