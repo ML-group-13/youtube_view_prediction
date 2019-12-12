3adb6c11421126b9c0bd2bf365e7da12b6e78382
XabiKrant <xabikrant@live.nl>
2019-12-12 11:30:16 +0100

    ADD: new measures for the model

--- pipe.py
+++ pipe.py
@@ -1,6 +1,7 @@
 import xgboost as xgb
-from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.metrics import mean_squared_error
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,7 +26,6 @@ def read_in_data():
 
 def read_images():
     images_data = []
-
     for file in [f for f in listdir("./data/images") if isfile(join("./data/images", f))]:
         images = np.load("./data/images/" + file, allow_pickle=True)
         for image in images['arr_0']:
@@ -34,25 +34,32 @@ def read_images():
 
 if __name__ == "__main__":
     data = read_in_data()
-    likes = data[['likes', 'dislikes', 'comment_total']]
-    views = data[['views']]
+    features = data[['likes', 'dislikes', 'comment_total']]
+    target = data[['views']]
 
-    data_dmatrix = xgb.DMatrix(data=likes, label=views)
+    data_dmatrix = xgb.DMatrix(data=features, label=target)
 
-    likes_train, likes_test, views_train, views_test = train_test_split(
-        likes, views, test_size=0.2, random_state=123)
+    x_train, x_test, y_train, y_test = train_test_split(
+        features, target, test_size=0.2, random_state=123)
 
-    xg_reg = xgb.XGBRegressor(objective='reg:linear', colsample_bytree=0.3, learning_rate=0.1,
-                              max_depth=5, alpha=10, n_estimators=10)
+    xgbr = xgb.XGBRegressor(objective='reg:linear', colsample_bytree=0.3, learning_rate=0.1,
+                            max_depth=5, alpha=10, n_estimators=10)
 
-    xg_reg.fit(likes_train, views_train)
+    xgbr.fit(x_train, y_train)
 
-    preds = xg_reg.predict(likes_test)
+    scores = cross_val_score(xgbr, x_train, y_train, cv=5)
+    print("Mean cross-validation score: %.2f" % scores.mean())
 
-    rmse = np.sqrt(mean_squared_error(views_test, preds))
-    print("RMSE: %f" % (rmse))
+    kfold = KFold(n_splits=10, shuffle=True)
+    kf_cv_scores = cross_val_score(xgbr, x_train, y_train, cv=kfold)
+    print("K-fold CV average score: %.2f" % kf_cv_scores.mean())
 
-    # Visualize feature importance
-    xgb.plot_importance(xg_reg)
+    predictions = xgbr.predict(x_test)
+    mse = mean_squared_error(y_test, predictions)
+    print("MSE: %.2f" % mse)
+    print("RMSE: %.2f" % np.sqrt(mse))
+
+	# Visualize feature importance
+    xgb.plot_importance(xgbr)
     plt.rcParams['figure.figsize'] = [5, 5]
     plt.show()
