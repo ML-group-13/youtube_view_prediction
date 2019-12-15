2acbe9ef3f7e3ba8bdd3183f6129258542eb28cc
XabiKrant <xabikrant@live.nl>
2019-12-15 16:27:35 +0100

    FIX: error in text feature extractor

--- feature_extraction/text_feature_extraction/text_feature_extractor.py
+++ feature_extraction/text_feature_extraction/text_feature_extractor.py
@@ -9,7 +9,7 @@ class TextFeatureExtractor:
         title_capitals_ratio = []
         title_non_letter_count = []
         title_non_letter_ratio = []
-        for title in df['title']:
+        for idx, title in df['title'].items():
             title_length.append(self.text_length(title))
             title_capitals_count.append(self.capitals_count(title))
             title_capitals_ratio.append(self.capitals_ratio(title))
@@ -26,14 +26,14 @@ class TextFeatureExtractor:
         return len(text)
 
     def capitals_count(self, text):
-        return sum(1 for c in input if c.isupper())
+        return sum(1 for c in text if c.isupper())
 
     def capitals_ratio(self, text):
         return self.capitals_count(text) / self.text_length(text)
 
     def non_letter_count(self, text):
         letters_and_space_regex = re.compile('[^a-zA-Z ]')
-        return len(re.findall(letters_and_space_regex, input))
+        return len(re.findall(letters_and_space_regex, text))
 
     def non_letter_ratio(self, text):
         return self.non_letter_count(text) / self.text_length(text)
